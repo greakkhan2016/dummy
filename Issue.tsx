a429d684d3b2a0c4c12bdbc79070f42741c3f5da
greakkhan2016 <seanthaughey@gmail.com>
2024-02-28 11:23:18 +0000

    Add files via upload

---
+++ Issue.tsx
@@ -0,0 +1,63 @@
+import React, { useCallback, useEffect } from 'react';
+
+import 'reactflow/dist/style.css';
+
+const nodeShapes = {
+  circle: CircleNode,
+  diamond: DiamondNode,
+  square: SquareNode,
+  roundedRectangle: RoundedRectangleNode,
+  pentagon: PentagonNode,
+  hexagon: HexagonNode,
+};
+
+const AttackReconstructionCanvas = () => {
+  // const { artifacts, isError, isLoading } = useBehavioralTree(storyID);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const onNodeClick = (event, node) => console.log('click node', node);
+  const onNodeMouseEnter = (event, node) => console.log('node hover', node);
+  const onNodeMouseLeave = (event, node) => console.log('stopped node hover', node);
+
+  const formatArtifactsForNodes = (artifacts) => {
+    
+  };
+
+  const formattEdgeForNodes = (lines) => {
+    
+  };
+
+
+  useEffect(() => {
+    setNodes(formatArtifactsForNodes(data.artifacts));
+    setEdges(formattEdgeForNodes(data.relationships));
+  }, []);
+
+  // Currently not displaying any connections/edges between nodes
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge({ ...params, animated: true, style: { stroke: '#fff' } }, eds)),
+    [setEdges],
+  );
+
+  return (
+    <div className='uax-canvas' data-testid='uax-canvas'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        fitView
+        onNodeClick={onNodeClick}
+        onNodeMouseEnter={onNodeMouseEnter}
+        onNodeMouseLeave={onNodeMouseLeave}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeShapes}
+        proOptions={{ hideAttribution: true }}>
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default AttackReconstructionCanvas;
